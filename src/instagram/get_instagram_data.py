2c6f19e0e3faf45e8fe76cec755485e92635d3f9
root <nicolas.hinderling@gmail.com>
2015-09-19 19:23:58 +0000

    updating some specifics

--- src/instagram/get_instagram_data.py
+++ src/instagram/get_instagram_data.py
@@ -1,7 +1,7 @@
 from firebase import firebase
 def retrive(suffix=''):
     app = firebase.FirebaseApplication('https://boiling-fire-6168.firebaseio.com/'+suffix, None)
-    return app.get('/twitter_data', None)
+    return app.get('/instagram_data', None)
 if __name__ == '__main__':
     #freeze_support()
     print(retrive())
--- src/twitter/twitter_kmeans.py
+++ src/twitter/twitter_kmeans.py
@@ -3,12 +3,11 @@ __author__ = 'Bill'
 import pandas as pd
 import json
 from get_twitter_data import retrive 
-instagram_data = retrive('instagram_data')
 twitter_data = retrive('twitter_data')
 
 exec(open("../../kmeans.py").read())
 
-df = pd.DataFrame.from_dict(instagram_data)
+df = pd.DataFrame.from_dict(twitter_data)
 
 data_labels,data_cluster_centers,data_num_each_cluster = kmeansData(df=df.transpose(),plotFlag=False)
 print(data_labels)
