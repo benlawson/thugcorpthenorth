0b8114fcceb1997a936010697adf529a63df61a3
NicoHinderling <nicolas.hinderling@gmail.com>
2015-09-19 18:09:09 -0400

    curl babe?0

--- src/twitter/twitter_kmeans.py
+++ src/twitter/twitter_kmeans.py
@@ -2,12 +2,17 @@ __author__ = 'Bill'
 
 import pandas as pd
 import json
+import os
 from get_twitter_data import retrive 
-twitter_data = retrive('twitter_data')
+from pprint import pprint
+# twitter_data = retrive('twitter_data')
+data = os.system('curl "https://boiling-fire-6168.firebaseio.com/.json?print=pretty"')
 
+# data = json.loads(json.dumps(twitter_data))
+pprint(data)
 exec(open("../../kmeans.py").read())
 
-df = pd.DataFrame.from_dict(twitter_data)
+df = pd.DataFrame.from_dict(data)
 
 data_labels,data_cluster_centers,data_num_each_cluster = kmeansData(df=df.transpose(),plotFlag=False)
 print(data_labels)
