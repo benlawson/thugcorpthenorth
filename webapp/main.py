8445589a1d534d29b98e932d61f77fbc77f1b6f3
Ian Edington <IanEdington@gmail.com>
2015-09-19 14:09:36 -0400

    add jinja2 to req, start using jinja2 for webapp

--- webapp/main.py
+++ webapp/main.py
@@ -1,10 +1,36 @@
 import os
 import webapp2
+import jinja2
 
 # from firebase import firebase
 # Example of firebase
 # firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
 # result = firebase.get('/users', None)
+
+main_form = """
+<form action="/map" method="post" id="mainform">
+  <h2>How far away?</h2>
+  <select name="Distance" form="mainform">
+    <option value="1">1 mile</option>
+    <option value="2">2 mile</option>
+    <option value="3">3 mile</option>
+    <option value="4">4 mile</option>
+  </select>
+  <div><textarea name="content" rows="3" cols="60"></textarea></div>
+  <div><input type="submit" value="Sign Guestbook"></div>
+</form>
+
+"""
+
+main_page =
+"""
+<html>
+<body>
+%s
+</body>
+</html>
+"""
+
 
 # handlers classes:
 class Handler(webapp2.RequestHandler):
@@ -14,7 +40,13 @@ class Handler(webapp2.RequestHandler):
 # Example of RequestHandler
 class MainPage(Handler):
     def get(self):
-        self.write('Hello, World!')
+        output = main_page % main_form
+        self.write(output)
+
+class MapPage(Handler):
+    def get(self):
+        output = map_page
+        self.write(output)
 
 
 app = webapp2.WSGIApplication([
