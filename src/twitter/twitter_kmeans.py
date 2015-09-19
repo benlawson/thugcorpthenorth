5c4b99b78197ac4b801b37a2cc771c666865f692
Bill <bill.zheng@nyu.edu>
2015-09-19 14:29:39 -0400

    twitter_kmeans

--- src/twitter/twitter_kmeans.py
+++ src/twitter/twitter_kmeans.py
@@ -3,14 +3,14 @@ __author__ = 'Bill'
 import pandas as pd
 
 import json
-exec(open("/Users/Bill/GitHub/thugcorpthenorth/kmeans.py").read())
+exec(open("../../kmeans.py").read())
 
 with open('boiling-fire-6168-data-export.json') as json_data:
     data = json.load(json_data)
 
 df = pd.DataFrame.from_dict(data)
 
-data_labels,data_cluster_centers,data_num_each_cluster = kmeansData(df=df.transpose(),plotFlag=True)
+data_labels,data_cluster_centers,data_num_each_cluster = kmeansData(df=df.transpose(),plotFlag=False)
 print(data_labels)
 print(data_cluster_centers)
 print(data_num_each_cluster)
