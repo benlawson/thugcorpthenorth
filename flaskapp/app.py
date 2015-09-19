68dbeb83259f23319d544dedc4bb0711f22bd53d
Ian Edington <IanEdington@gmail.com>
2015-09-19 18:53:42 -0400

    refference templates

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -2,9 +2,11 @@ import os
 import json
 from flask import Flask, request, redirect, render_template, url_for
 
+app = Flask(__name__)
+
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template('referenceindex.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
