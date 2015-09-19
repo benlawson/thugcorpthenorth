797cab963c7826e26cc70a3bc2f053e9d079c709
NicoHinderling <nicolas.hinderling@gmail.com>
2015-09-19 19:44:30 -0400

    small bug fix

--- src/instagram/instagram_scrapper.py
+++ src/instagram/instagram_scrapper.py
@@ -13,22 +13,21 @@ firebase = firebase.FirebaseApplication('https://boiling-fire-6168.firebaseio.co
 
 def send_data(response):
     query = {}
-    for x in range(len(response)):
-        query["coordinate_1"] = response[x]['location']['latitude']
-        query["coordinate_2"] = response[x]['location']['longitude']
-        query["created_at"] = datetime.datetime.fromtimestamp(int(response[x]['created_time'])).strftime('%Y-%m-%d %H:%M:%S')
-        query["id"] = response[x]['id']
+    query["coordinate_1"] = response[x]['location']['latitude']
+    query["coordinate_2"] = response[x]['location']['longitude']
+    query["created_at"] = datetime.datetime.fromtimestamp(int(response[x]['created_time'])).strftime('%Y-%m-%d %H:%M:%S')
+    query["id"] = response[x]['id']
 
-        if 'caption' in response[x]:
-            if response[x]['caption'] != None:
-                if 'text' in response[x]['caption']:
-                    query["text"] = response[x]['caption']['text']
+    if 'caption' in response[x]:
+        if response[x]['caption'] != None:
+            if 'text' in response[x]['caption']:
+                query["text"] = response[x]['caption']['text']
 
-        query["img"] = response[x]['images']['standard_resolution']
+    query["img"] = response[x]['images']['standard_resolution']
 
-        if 'videos' in response[x]:
-            query["video"] = response[x]['videos']['standard_resolution']
-        result = firebase.post('/instagram_data', query)
+    if 'videos' in response[x]:
+        query["video"] = response[x]['videos']['standard_resolution']
+    result = firebase.post('/instagram_data', query)
 
 
 http = urllib3.PoolManager()
@@ -36,5 +35,6 @@ http = urllib3.PoolManager()
 while True:
     response = http.request('GET', "https://api.instagram.com/v1/media/search?lat=43.653226&lng=-79.383184&distance=500000&access_token=15936371.e012851.5d851e6e8e0446b2bf66ecb70da6a9d5").data
     info = json.loads(response)['data']
-    if info[x]['location'] != None:
-        send_data(info)
+    for x in range(len(response)):
+        if info[x]['location'] != None:
+            send_data(info)
